096f3cb7db25b7ab73dfb71759294a27fb7db7aa
FaithMaria9840 <78792585+FaithMaria9840@users.noreply.github.com>
2021-04-27 10:16:46 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+/*Write a function that plays a single round of Rock Paper Scissors. The function should take two parameters -
+the playerSelection and computerSelection - and then return a string that declares the winner of the round 
+like so: "You Lose! Paper beats Rock"
+Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any 
+other variation).*/
+/*Write a NEW function called game(). Use the previous function inside of this one to play a 5 round game that 
+keeps score and reports a winner or loser at the end.*/
+
+function computerPlay() {
+    return 'Rock' || 'Paper' || 'Scissors';
+}
+
+
+    function playRound(playerSelection, computerSelection) {
+    if (playerSelection  === 'Scissors' && computerSelection === 'Paper' ||
+    playerSelection  === 'Paper' && computerSelection === 'Rock' ||
+    playerSelection  === 'Rock' && computerSelection === 'Scissors') {
+            return 'you win';
+       }else if (playerSelection  === 'Paper' && computerSelection === 'Scissors' ||
+       playerSelection  === 'Rock' && computerSelection === 'Paper' ||
+       playerSelection === 'Scissors' && computerSelection === 'Rock') {
+           return 'you lose';
+       }else {
+           return 'a tie';
+       }
+    }
+    function game() {
+        let playerScore = 0;
+        let computerScore = 0;
+
+        for (let i=0; i < 5; i++) {
+            const playerSelection = prompt("Rock, Paper, or Scissors?");
+            const computerSelection = computerPlay();
+            console.log(`Computer played "${computerSelection}"`);
+            const consoleOutput = playRound(playerSelection, computerSelection);
+            console.log(consoleOutput);
+            if (consoleOutput.includes("win", 3)) {
+                ++playerScore;
+              }else {
+                ++computerScore;
+              }
+            }
+            if (playerScore < computerScore){
+              console.log("You lose the game.");
+            } else if (playerScore > computerScore) {
+              console.log("You win the game");
+            } else if(playerScore === computerScore) {
+              console.log("You are tied");
+            }
+            }
+    
+ 
